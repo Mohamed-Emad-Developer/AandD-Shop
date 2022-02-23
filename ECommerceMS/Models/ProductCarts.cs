3a23866851591ebdbe463eef7bd0e69baf86601d
Mohamed Emad <Mohamed-Emad-Developer>
2022-02-23 23:12:53 +0200

    Add Quantity Attribute To 'ProductCarts' Table

--- ECommerceMS/Models/ProductCarts.cs
+++ ECommerceMS/Models/ProductCarts.cs
@@ -9,7 +9,7 @@ namespace ECommerceMS.Models
 
         [ForeignKey("Cart")]
         public int CartId { get; set; }
-
+        public int Quantity { get; set; }
         public Product Product { get; set; }
         public Cart Cart { get; set; }
     }
