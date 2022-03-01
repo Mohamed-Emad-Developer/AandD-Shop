3e25534cc43641d339c3dbb0ddcc342189168146
Mohamed Emad <Mohamed-Emad-developer>
2022-03-02 01:53:50 +0200

    Edit Layout

--- ECommerceMS/Controllers/CustomerController.cs
+++ ECommerceMS/Controllers/CustomerController.cs
@@ -1,21 +1,31 @@
 ﻿using ECommerceMS.services.repository;
+using ECommerceMS.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceMS.Controllers
 {
+    [Authorize(Roles = "Customer")]
     public class CustomerController : Controller
     {
         private readonly ICustomerRepository _customerRepository;
+        private readonly IOrderRepository _orderRepository;
 
-        public CustomerController(ICustomerRepository customerRepository)
+        public CustomerController(ICustomerRepository customerRepository, IOrderRepository orderRepository)
         {
             _customerRepository = customerRepository;
+            _orderRepository = orderRepository;
         }
 
         public IActionResult Account(string id)
         {
             var customer = _customerRepository.Get(id);
-            return View(customer);
+            var customerDetailsVM = new CustomerAccountViewModel()
+            {
+                Cusomer = customer,
+                Orders = _orderRepository.GetByCusID(id)
+            };
+            return View(customerDetailsVM);
         }
     }
 }
---
+++ ECommerceMS/ViewModel/CustomerAccountViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ECommerceMS.Models;
+using System.Collections.Generic;
+
+namespace ECommerceMS.ViewModel
+{
+    public class CustomerAccountViewModel
+    {
+        public Customer Cusomer { get; set; }
+        public List<Order> Orders { get; set; }
+    }
+}
