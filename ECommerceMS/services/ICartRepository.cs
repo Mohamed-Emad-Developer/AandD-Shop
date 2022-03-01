aa63c895e8e0ba6ade3b89da464864eeabc967d0
Mohamed Emad <Mohamed-Emad-developer>
2022-03-01 18:18:21 +0200

    Edit Cart Controller

--- ECommerceMS/services/ICartRepository.cs
+++ ECommerceMS/services/ICartRepository.cs
@@ -5,5 +5,10 @@ namespace ECommerceMS.services
     public interface ICartRepository
     {
         Cart Get(int id);
+        int Create(string customerId);
+        int AddPrpductToCart(int prodcutId, string customerId);
+        Cart GetByUserId(string customerId);
+        void IncrementQuantity(int cartId);
+        void DecrementQuantity(int cartId);
     }
 }
--- ECommerceMS/services/repository/CartRepository.cs
+++ ECommerceMS/services/repository/CartRepository.cs
@@ -5,16 +5,67 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceMS.services.repository
 {
-    public class CartRepository: ICartRepository
+    public class CartRepository : ICartRepository
     {
-        readonly ECommerceDB DBContext;
+        private readonly ECommerceDB DBContext;
         public CartRepository(ECommerceDB _DBContext)
         {
             DBContext = _DBContext;
         }
         public Cart Get(int id)
         {
-            return DBContext.Carts.Include(x=>x.ProductCarts).ThenInclude(x=>x.Product).FirstOrDefault(x=>x.Id==id);
+            return DBContext.Carts.Include(x => x.ProductCarts).ThenInclude(x => x.Product).FirstOrDefault(x => x.Id == id);
         }
+        public int Create(string customerId)
+        {
+            var cart = new Cart() { CustomerId = customerId };
+            DBContext.Carts.Add(cart);
+            return DBContext.SaveChanges();
+        }
+        public int AddPrpductToCart(int prodcutId, string customerId)
+        {
+            var cartInDb = DBContext.Carts.FirstOrDefault(c => c.CustomerId == customerId);
+            var productInDb = DBContext.Products.FirstOrDefault(p => p.Id == prodcutId);
+            var prodcutCartInDB = DBContext.ProductCarts.Where(pc => pc.ProductId == prodcutId && pc.CartId == cartInDb.Id);
+            if (!(prodcutCartInDB.Count() > 0))
+            {
+                var prodcutCart = new ProductCarts() { Quantity = 1, ProductId = prodcutId, CartId = cartInDb.Id };
+
+                productInDb.ProductCarts.Add(prodcutCart);
+                cartInDb.ProductCarts.Add(prodcutCart);
+            }
+            return DBContext.SaveChanges();
+        }
+
+
+
+        public Cart GetByUserId(string customerId)
+        {
+            var cartInDb = DBContext.Carts.Include(c => c.ProductCarts).ThenInclude(pc => pc.Product).FirstOrDefault(c => c.CustomerId == customerId);
+            return cartInDb;
+        }
+
+        public void IncrementQuantity(int cartId)
+        {
+            var productCartInDb = DBContext.ProductCarts.Include(pc=>pc.Product).FirstOrDefault(pc => pc.CartId == cartId);
+            if (productCartInDb.Quantity < productCartInDb.Product.StockQuantity)
+                productCartInDb.Quantity++;
+            DBContext.SaveChanges();
+        }
+
+        public void DecrementQuantity(int cartId)
+        {
+            var productCartInDb = DBContext.ProductCarts.FirstOrDefault(pc => pc.CartId == cartId);
+            if (productCartInDb.Quantity > 1)
+                productCartInDb.Quantity--;
+            DBContext.SaveChanges();
+        }
+        //public void RemoveFromCart(int cartId,int productId)
+        //{
+        //    var productCartInDb = DBContext.ProductCarts.FirstOrDefault(pc => pc.CartId == cartId);
+        //    if (productCartInDb.Quantity > 1)
+        //        productCartInDb.Quantity--;
+        //    DBContext.SaveChanges();
+        //}
     }
 }
