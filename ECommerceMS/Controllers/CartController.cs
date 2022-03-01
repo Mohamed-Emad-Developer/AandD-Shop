e6cfc3f79a6b53b4a533387e60c6f5ebe754e292
Mohamed Emad <Mohamed-Emad-developer>
2022-03-01 18:16:33 +0200

    Add Cart Controller

---
+++ ECommerceMS/Controllers/CartController.cs
@@ -0,0 +1,41 @@
+﻿using ECommerceMS.Models;
+using ECommerceMS.services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMS.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartRepository _cartRepository;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CartController(ICartRepository cartRepository, UserManager<ApplicationUser> userManager)
+        {
+            _cartRepository = cartRepository;
+            _userManager = userManager;
+        }
+        //int AddPrpductToCart(int prodcutId, string customerId);
+        public IActionResult AddToCart(int id)
+        {
+            _cartRepository.AddPrpductToCart(id, _userManager.GetUserId(User));
+            return RedirectToAction("CartDetails");
+
+        }
+        public IActionResult CartDetails()
+        {
+            var cartInDb = _cartRepository.GetByUserId(_userManager.GetUserId(User));
+            return View(cartInDb);
+        }
+        public IActionResult IncrementQuantity(int id)
+        {
+           _cartRepository.IncrementQuantity(id);
+            return RedirectToAction("CartDetails");
+        }
+        public IActionResult DecrementQuantity(int id)
+        {
+           _cartRepository.DecrementQuantity(id);
+            return RedirectToAction("CartDetails");
+        }
+    }
+}
