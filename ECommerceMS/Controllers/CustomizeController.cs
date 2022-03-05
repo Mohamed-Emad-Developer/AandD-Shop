dcbb8a7a5c2ab68763274babf12cdb91d440ee17
Mohamed Emad <Mohamed-Emad-developer>
2022-03-05 03:07:42 +0200

    Demo For Customize Product

---
+++ ECommerceMS/Controllers/CustomizeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMS.Controllers
+{
+    public class CustomizeController : Controller
+    {
+        public IActionResult Style()
+        {
+            return View();
+        }
+    }
+}
